f577cde6860b046a996722a23da440d5519a6560
Ervant <Ervant@users.noreply.github.com>
2015-11-08 14:50:46 -0800

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,53 @@
+# Exploratory Data Analysis
+#Project 1
+
+library(ggplot2)
+
+# OSX
+getwd()
+setwd("~/Documents/Data_Science_Specialization/ExploratoryDataAnalysis")
+
+# Import data 
+my_data <- read.table("household_power_consumption.txt", sep=";", header=TRUE)
+
+# Create new data frame "my_data2" with combined Date and Time
+my_data$DateTime = paste(my_data$Date, my_data$Time, sep = " ")
+
+# Transform to Date-Time format
+my_data$DateTime_fmt <- strptime(my_data$DateTime, format="%d/%m/%Y %H:%M:%S")
+
+# Add Day based on given date
+my_data$Day <- weekdays(my_data$DateTime_fmt)
+
+head(my_data)
+# Organize columns
+my_data <- my_data[, c(1,2,10,11,12,3,4,5,6,7,8,9)]
+head(my_data) #looks better
+
+# Subset imported data set [raw_data] for Date = 2007-02-01 and 2007-02-02
+data_small <- subset(my_data, DateTime_fmt >= as.POSIXct('2007-02-01 00:00:00') & DateTime_fmt <= as.POSIXct('2007-02-02 23:59:59'))
+
+# Transform variables from Factor type to Numeric type
+data_small$Global_active_power <- as.numeric(as.character(data_small$Global_active_power))
+data_small$Global_reactive_power <- as.numeric(as.character(data_small$Global_reactive_power))
+data_small$Voltage <- as.numeric(as.character(data_small$Voltage))
+data_small$Global_intensity <- as.numeric(as.character(data_small$Global_intensity))
+data_small$Sub_metering_1 <- as.numeric(as.character(data_small$Sub_metering_1))
+data_small$Sub_metering_2 <- as.numeric(as.character(data_small$Sub_metering_2))
+data_small$Sub_metering_3 <- as.numeric(as.character(data_small$Sub_metering_3))
+
+# Plot 4 Energy panel illustration
+par(mfrow = c(2,2))
+# plot 1 (top-left)
+plot(data_small$DateTime_fmt, data_small$Global_active_power, type = "l", main="", xlab="", ylab="Global Active Power (kilowatts)")
+# plot 2 (top-right)
+plot(data_small$DateTime_fmt, data_small$Voltage, type = "l", main = "", xlab="datetime", ylab = "Voltage")
+# plot 3 (bottom-left)
+plot(data_small$DateTime_fmt, data_small$Sub_metering_1, type = "l", main="", xlab="", ylab="Energy sub metering", col="black")
+lines(data_small$DateTime_fmt, data_small$Sub_metering_2, col = "red")
+lines(data_small$DateTime_fmt, data_small$Sub_metering_3, col = "blue")
+legend("topright", pch = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+# plot 4 (bottom-right)
+plot(data_small$DateTime_fmt, data_small$Global_reactive_power, type = "l", main="", xlab="datetime", ylab="Global_Reactive_power")
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off()
